ada6c26864c886af1f586e71b804351e4c3b1be8
hakdo <hakdo@outlook.com>
2016-08-18 15:21:07 +0200

    public liste implementert

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -32,16 +32,27 @@ def createlist(request):
 @login_required(login_url='/login/')
 def list_edit(request,pk):
     current_list = get_object_or_404(SimpleList, pk=pk)
-    if request.method == "POST":
-        form = SimpleListForm(request.POST, instance=current_list)
-        if form.is_valid():
-            # Commit = False in order to process data later if needed
-            current_list = form.save(commit=False)
-            current_list.save()
-            return redirect('listdetail', pk = current_list.pk)
+    print('OK, I got the list.')
+    if request.user.is_authenticated():
+        if current_list.owner == request.user.username or current_list.sharedwith == request.user.username:
+            if request.method == "POST":
+                form = SimpleListForm(request.POST, instance=current_list)
+                if form.is_valid():
+                    print('and the post request is valid')
+                    # Commit = False in order to process data later if needed
+                    current_list = form.save(commit=False)
+                    current_list.save()
+                    return redirect('listdetail', pk = current_list.pk)
+            else:
+                form = SimpleListForm(instance=current_list)
+                return render(request, 'shoppinglist/createlist.html', {'form': form})
+        elif current_list.sharedwith == 'public':
+            print('public list, user is authenticated')
+            #return redirect('list_detail', pk = current_list.pk)
+            list_items=current_list.contents.split('\n')
+            return render(request,'shoppinglist/list_detail.html', {'list': current_list, 'items': list_items})
     else:
-        form = SimpleListForm(instance=current_list)
-    return render(request, 'shoppinglist/createlist.html', {'form': form})
+        return redirect('list_detail', pk = current_list.pk)
 
 
 
@@ -56,34 +67,41 @@ def yourshoppinglists(request):
     shopping_lists = SimpleList.objects.filter(finished=False, sharedwith=request.user.username)
     return render(request, 'shoppinglist/my_shopping_lists.html', {'lists': shopping_lists, 'ownership': 'shared'})
 
-@login_required(login_url='/login/')
+#@login_required(login_url='/login/')
 def list_detail(request, pk):
     current_list = get_object_or_404(SimpleList, pk=pk)
-    if request.method =='POST':
-        if current_list.owner == request.user.username or current_list.sharedwith == request.user.username:
-            collect_keys=[]
-            shopping_list = current_list.contents.split('\r\n')
-            for key in request.POST:
-                collect_keys.append(''.join(key.split('\r\n')))
-            for key in collect_keys:
-                if key in shopping_list:
-                    try:
-                        shopping_list = [ x for x in shopping_list if x != key]
-                    except:
-                        pass
-            current_list.contents = '\r\n'.join(shopping_list)
-            if len(shopping_list)==0:
-                current_list.finished = True
-            current_list.save()
-            return redirect('myshoppinglists')
-        else:
-            return HttpResponse('Wrong User - No Access')
-    else:
-        if current_list.owner == request.user.username or current_list.sharedwith == request.user.username:
+    if not request.user.is_authenticated():
+        if current_list.sharedwith == 'public':
             list_items=current_list.contents.split('\n')
             return render(request, 'shoppinglist/list_detail.html', {'list': current_list, 'items': list_items})
         else:
-            return HttpResponse('Wrong User - No Access')
+            return HttpResponse('Du har forsøkt å nå en side som krever innlogging, eller en side som ikke eksisterer.')
+    else:
+        if request.method =='POST':
+            if current_list.owner == request.user.username or current_list.sharedwith == request.user.username:
+                collect_keys=[]
+                shopping_list = current_list.contents.split('\r\n')
+                for key in request.POST:
+                    collect_keys.append(''.join(key.split('\r\n')))
+                for key in collect_keys:
+                    if key in shopping_list:
+                        try:
+                            shopping_list = [ x for x in shopping_list if x != key]
+                        except:
+                            pass
+                current_list.contents = '\r\n'.join(shopping_list)
+                if len(shopping_list)==0:
+                    current_list.finished = True
+                current_list.save()
+                return redirect('myshoppinglists')
+            else:
+                return HttpResponse('Wrong User - No Access ')
+        else:
+            if current_list.owner == request.user.username or current_list.sharedwith == request.user.username or current_list.sharedwith == 'public':
+                list_items=current_list.contents.split('\n')
+                return render(request, 'shoppinglist/list_detail.html', {'list': current_list, 'items': list_items})
+            else:
+                return HttpResponse('Wrong User - No Access')
 
 def userlogin(request):
     if request.method == 'POST':
@@ -92,8 +110,14 @@ def userlogin(request):
             user = authenticate(username=userdata.cleaned_data['username'],password=userdata.cleaned_data['password'])
             if user is not None:
                 if user.is_active:
+                    print(request.GET)
+                    #nextviewname = request.GET['next']
                     login(request,user)
-                    return redirect('init')
+                    try:
+                        nextviewname = request.GET['next']
+                    except:
+                        nextviewname = 'init'
+                    return redirect(nextviewname)
                 else:
                     return HttpResponse('Konto er ikke aktiv.')
             else:
